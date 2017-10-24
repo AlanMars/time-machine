8599efc788d2e9c2969238f5d2d15bc49e5fa995
Alan Yin <Alan.Yin@autodesk.com>
2017-10-24 13:52:40 +0800

    Create POC video (USS) - adding assets - images and audio

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var videoshow = require('videoshow')
 
 var images = [
-    __dirname + '/asset/images/step1.jpg',
-    __dirname + '/asset/images/step2.jpg',
-    __dirname + '/asset/images/step3.jpg',
-    __dirname + '/asset/images/step4.jpg'
+    __dirname + '/asset/images/1.jpg',
+    __dirname + '/asset/images/2.jpg',
+    __dirname + '/asset/images/3.jpg',
+    __dirname + '/asset/images/4.jpg',
+    __dirname + '/asset/images/5.jpg',
+    __dirname + '/asset/images/6.jpg',
+    __dirname + '/asset/images/7.jpg',
+    __dirname + '/asset/images/8.jpg',
+    __dirname + '/asset/images/9.jpg',
+    __dirname + '/asset/images/10.jpg'
 ]
 
-var audio = __dirname + '/asset/audios/medium-Life is  AmPm feat.Hiro-A-Key.mp3'
+var audio = __dirname + '/asset/audios/slow-shape of you lyrics.mp3'
 
-var outputVideo = __dirname + '/output/video.mp4'
+var outputVideo = __dirname + '/output/USS-video.mp4'
 
 var videoOptions = {
     fps: 25,
@@ -18,7 +24,7 @@ var videoOptions = {
     transitionDuration: 1, // seconds
     videoBitrate: 1024,
     videoCodec: 'libx264',
-    size: '640x?',
+    size: '1920x1080', //'640x?',
     audioBitrate: '128k',
     audioChannels: 2,
     format: 'mp4',
